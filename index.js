bed1efe3143fc006a24085084d3934558847160a
Andrew Truett <andrewtruett@aol.com>
2021-08-01 20:27:31 -0400

    Init commit

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
 const express = require('express');
-const request = require('request-promise');
+//const request = require('request-promise');
+const redis = require("redis");
 const cheerio = require('cheerio');
 const axios = require('axios');
 const _ = require('lodash');
+const schedule = require('node-schedule');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+//Redis config
+/*
+const redisPort = 6379
+const client = redis.createClient(redisPort);
+const CACHE_EXP = 60 * 60 * 24
+
+client.flushdb( function (err, succeeded) {
+    console.log(succeeded); // will be true if successfull
+});
+
+client.on("error", (err) => {
+    console.log(err);
+})
+*/
+
+let minerals = {};
+
+async function readMinerals() {
+    console.log("Lets dig up some minerals");
+    let mineralsXpath = "//*[@id='A']//parent::h2/following-sibling::*[@class='div-col'][1]//a";
+    let qMineralsXpath = "//*[@id='Q']//parent::h2/following-sibling::ul[1]//a";
+
+    const MINERAL_LIST_URL = 'https://en.wikipedia.org/wiki/List_of_minerals';
+
+    const { data } = await axios.get(MINERAL_LIST_URL);
+	const $ = cheerio.load(data);
+
+    const alphabet = 'ABCDEFGHIJKLMNOPRSTUVWXYZ'; //is missing Q because Wikipedia displays the Q minerals differently
+
+    for(let i = 0; i < alphabet.length; i++) {
+        let id = '#' + alphabet[i];
+        console.log(id);
+
+        $('#A').parent().nextAll('.div-col:first-of-type, ul:first-of-type').find('li a').each((_idx, el) => {
+            console.log($(el).text())
+        });
+
+    }
+}
+
+const job = schedule.scheduleJob('* * * * *', readMinerals);
 
 app.use(express.json());
 
@@ -13,6 +57,43 @@ app.get('/mineral/:mineralName', async (req, res) => {
     const { mineralName } = req.params;
 
     const BASE_URL = 'https://en.wikipedia.org/wiki/';
+
+    //** */
+    try {
+        client.get(mineralName, async (err, mineralData) => {
+            if (err) throw err;
+    
+            if (mineralData) {
+                res.status(200).send(JSON.parse(mineralData));
+            }
+            else {
+                console.log("Cache miss on", mineralName);
+                const { data } = await axios.get(BASE_URL + `${mineralName}`);
+		        const $ = cheerio.load(data);
+
+                let mineralData = {};
+
+                $('.infobox tbody .infobox-label').parent().each((_idx, el) => {
+                    let key = _.camelCase($(el).find('.infobox-label').text())
+                    let value = $(el).find('.infobox-data').text()
+
+                    if(key.toLowerCase().includes('references')) 
+                        return;
+            
+                    mineralData[key] = value
+                });
+
+                mineralData['imageUrl'] = $('.infobox-image img').attr('src')
+
+
+                client.setex(mineralName, String(CACHE_EXP), JSON.stringify(mineralData));
+                res.status(200).send(mineralData);
+            }
+        });
+    } catch(err) {
+        res.status(500).send({message: err.message});
+    }
+    /*
 
     try {
         const { data } = await axios.get(BASE_URL + `${mineralName}`);
@@ -35,52 +116,11 @@ app.get('/mineral/:mineralName', async (req, res) => {
         console.log(mineralData)
         res.send(mineralData)
 
-
-
-
-
-
-
-
-
-
-
-        /*
-        const { data } = await axios.get(
-			BASE_URL + `${mineralName}.shtml`
-		);
-		const $ = cheerio.load(data);
-
-        const fieldsToGet = ['Chemical Classification', 'Color', 'Streak', 'Luster', 'Diaphaneity', 'Cleavage', 'Mohs Hardness', 'Specific Gravity', 'Diagnostic Properties', 'Chemical Composition', 'Crystal System', 'Uses']
-
-        let mineralData = {};
-
-        fieldsToGet.forEach( (field, i) => {
-            $(`b:contains("${field}")`).parent().next().each((_idx, el) => {
-                let camelCase = _.camelCase(field)
-                mineralData[camelCase] = $(el).text();
-            });
-        })
-
-        //description from first paragraph
-        $('h2:contains("What is")').next().each((_idx, el) => {
-            mineralData["description"] = $(el).text();
-        });
-        
-        //images
-        let images = []
-        $('img[src*="photos/"]').each((_idx, el) => {
-            images.push(BASE_URL + $(el).attr('src'));
-        });
-
-        mineralData["images"] = images
-        console.log(mineralData)
-        res.send(mineralData)
-        */
-
 	} catch (error) {
+        // res.status(500).send({message: error.message});
 		throw error;
 	}
+    */
 })
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
